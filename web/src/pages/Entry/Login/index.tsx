fb3a05146bd82a69080b23d5eee40457454bbc53
João Pedro Alves <jotalmeida007@hotmail.com>
2020-11-12 20:21:28 -0300

    Add icons do login screen

--- web/src/pages/Entry/Login/index.tsx
+++ web/src/pages/Entry/Login/index.tsx
@@ -2,18 +2,28 @@ import React from 'react';
 import './styles/Login.css';
 import '../Entry.css';
 import { BsEyeSlash } from 'react-icons/bs';
+import { BsArrowLeft } from 'react-icons/bs';
+import { Link } from 'react-router-dom';
 
 export default function Login() {
   return (
     <div className="background">
       <div className="loginBackground">
         <div className="entryContainer">
+          <div className="GoBackEntryIcon">
+            <Link to="/">
+              <BsArrowLeft color="white" size={50} />
+            </Link>
+          </div>
+
           <p>Login</p>
           <form action="" className="loginInputs">
             <input type="email" placeholder="Email" name="" id="" />
-            <input type="password" placeholder="Password" name="" id="" />
-            {/* <BsEyeSlash size={32} color="white" /> */}{' '}
-            {/* Eyes icon to reveal password ^ */}
+            <div className="passwordContainer">
+              <input type="password" placeholder="Password" name="" id="" />
+              <BsEyeSlash size={32} color="white" className="showPassword" />
+            </div>
+
             <input type="submit" value="LOGIN" />
           </form>
           <div className="loginLinks">
