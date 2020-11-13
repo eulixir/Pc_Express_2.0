ed5ba3c7b79313e82b1c7a337afb2dc30605cb59
João Pedro Alves <jotalmeida007@hotmail.com>
2020-11-13 20:46:17 -0300

    Start backend to 0

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const mongoose = require('mongoose');
-const routes = require('./routes/routes.js');
-const path = require('path');
-const dotenv = require('dotenv');
 
-app.use(express.json());
+const Nhaa = [];
 app.use(cors());
+app.use(express.json());
+
+app.post('/Register', (request, response) => {
+  const { name, email, password } = request.body;
+
+  const register = { name, email, password };
+
+  Nhaa.push(register);
+  console.log('----------------------------------------------------------');
+  console.log(register);
+  console.log('----------------------------------------------------------');
+  return response.json(register);
+});
+
+app.get('./Entry/Login'), (request, response) => {};
 
 console.log('----------------------------------------------------------');
-console.log('Backend Started :3');
+console.log('|                   Backend Started ✔                    |');
+console.log('----------------------------------------------------------');
 
 module.exports = app;
