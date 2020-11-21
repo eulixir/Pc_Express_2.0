6e957198c9ff6c8a22876ac0a07245de9043afa5
João Pedro Alves <jotalmeida007@hotmail.com>
2020-11-20 22:51:52 -0300

    Link front end page to backend

--- web/src/pages/Entry/ForgotPassword/index.tsx
+++ web/src/pages/Entry/ForgotPassword/index.tsx
@@ -1,10 +1,29 @@
-import React from 'react';
-
+import React, { useState, FormEvent } from 'react';
 import './styles/ForgotPassword.css';
 import { BsArrowLeft } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
+import api from '../../../services/api';
+import { useHistory } from 'react-router-dom';
 
 export default function ForgotPassword() {
+  const history = useHistory();
+  const [email, setEmail] = useState('');
+
+  console.log(email);
+  function handleForgotPassword(e: FormEvent) {
+    e.preventDefault();
+
+    api
+      .get('Entry/sendEmail/' + email, {})
+      .then(() => {
+        window.alert('A verification code has been sent to your email');
+        // history.push('/');
+      })
+      .catch((err) => {
+        window.alert('Invalid email');
+      });
+  }
+
   return (
     <div className="background">
       <div className="forgotBackground">
@@ -20,17 +39,15 @@ export default function ForgotPassword() {
             Forgot password? does not matter! We will send a code in your email
             !
           </h6>
-          <form action="" className="forgotInputs">
+          <form onSubmit={handleForgotPassword} className="forgotInputs">
             <input
               type="email"
               placeholder="Your email registered"
-              name=""
-              id=""
+              value={email}
+              onChange={(e) => [setEmail(e.target.value)]}
+              required
             />
-
-            <Link to="/Entry/ChangePassword">
-              <input type="submit" value="SEND CODE TO YOUR EMAIL" />
-            </Link>
+            <input type="submit" value="SEND CODE TO YOUR EMAIL" />
           </form>
         </div>
       </div>
