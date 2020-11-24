f5625d3d33a49fc32e40650a8b4a57849a59a9f4
João Pedro Alves <jotalmeida007@hotmail.com>
2020-11-24 11:53:21 -0300

    create navbar

--- web/src/components/navbar/index.tsx
+++ web/src/components/navbar/index.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { IoIosMenu } from 'react-icons/io';
+import { BsSearch } from 'react-icons/bs';
+import { RiUserFill } from 'react-icons/ri';
+import { ImCart } from 'react-icons/im';
+import './styles/navbar.css';
 
 const Navbar = () => {
   return (
     <div>
-      <div className="teste">Opa</div>
+      <div className="navbarContainer">
+        <div className="landingMenu">
+          <IoIosMenu size={50} color={'#294CCA'} />
+        </div>
+        <div className="landingLogo">
+          <p>Pc Express</p>
+        </div>
+        <div className="searchLogo">
+          <BsSearch size={25} color={'#fff'} />
+        </div>
+        <div className="landingUser">
+          <RiUserFill size={25} color={'#294CCA'} />
+        </div>
+        <div className="marketcarLanding">
+          <ImCart size={25} color={'#fff'} />
+        </div>
+      </div>
     </div>
   );
 };
