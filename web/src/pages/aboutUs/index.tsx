f602f6dff4b70f8bde61998f865e3c847293cce0
João Pedro Alves <jotalmeida007@hotmail.com>
2020-12-23 17:02:08 -0300

    Update aboutUs page

--- web/src/pages/aboutUs/index.tsx
+++ web/src/pages/aboutUs/index.tsx
@@ -24,6 +24,30 @@ export default function aboutUs() {
             </Link>
           </div>
         </div>
+        <div className="mvvHeader">
+          <span>MISSION, VISION AND VALUES</span>
+        </div>
+        <div className="mvvItensContainer">
+          <div className="mvvItens">
+            <ImFlag size={32} color={'#fff'} />
+            <h5>Mission</h5>
+            <span>
+              Help those who need a machine and don't know where to start
+            </span>
+          </div>
+          <div className="mvvItens">
+            <ImEye size={32} color={'#fff'} />
+            <h5>Vision</h5>
+            <span>Be a reference in computer assembly consulting.</span>
+          </div>
+          <div className="mvvItens">
+            <FaHandshake size={32} color={'#fff'} />
+            <h5>Values</h5>
+            <span>* Respect and Appreciation of people</span>
+            <span>* Proactivity and Altruism</span>
+            <span>* Innovation and Leadership</span>
+          </div>
+        </div>
         <div className="abousUsContainer">
           {/* MVV = Mission, Vision and Values */}
           <div className="creationContainer">
@@ -41,40 +65,27 @@ export default function aboutUs() {
               <h5>How did the company Pc_Express come about?</h5>
               <span>
                 From an idea of ​​our CEO, he had this idea in mind at the
-                beginning of his college, and even designed the
-                {/* <a target="_blank" href="../pc_expressV1/index.html"> */}
-                first prototype of Pc_Express
-                {/* </a> */}, but left it aside due to limitations of human
-                capacity at the time, however, a while later a opportunity
-                opportunity to resume the project in a college job, but the
-                programming language he was given was unpleasant in his words,
-                and finally, after months of studying, he started to build this
-                interface that you see, more pleasant, isn't it?
+                beginning of his college, and even designed the first prototype
+                of Pc_Express, you can see{' '}
+                <a
+                  target="_blank"
+                  href="https://joaopealves.github.io/PC-express-Alpha/"
+                >
+                  here
+                </a>
+                , but left it aside due to limitations of human capacity at the
+                time, however, a while later a opportunity to resume the project
+                in a college task, but the programming language he was given was
+                unpleasant in his words, you can check a second version{' '}
+                <a
+                  target="_blank"
+                  href="https://joaopealves.github.io/Pc_Express_1.0/"
+                >
+                  here
+                </a>
+                . and finally, after months of studying, he started to build
+                this interface that you see, more pleasant, isn't it?
               </span>
-            </div>
-          </div>
-          <div className="mvvHeader">
-            <span>MISSION, VISION AND VALUES</span>
-          </div>
-          <div className="mvvItensContainer">
-            <div className="mvvItens">
-              <ImFlag size={32} color={'#fff'} />
-              <h5>Mission</h5>
-              <span>
-                Help those who need a machine and don't know where to start
-              </span>
-            </div>
-            <div className="mvvItens">
-              <ImEye size={32} color={'#fff'} />
-              <h5>Vision</h5>
-              <span>Be a reference in computer assembly consulting.</span>
-            </div>
-            <div className="mvvItens">
-              <FaHandshake size={32} color={'#fff'} />
-              <h5>Values</h5>
-              <span>* Respect and Appreciation of people</span>
-              <span>* Proactivity and Altruism</span>
-              <span>* Innovation and Leadership</span>
             </div>
           </div>
         </div>
