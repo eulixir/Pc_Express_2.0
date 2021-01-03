f4d2308c2de379095932607f723780e100aa3f59
João Pedro Alves <jotalmeida007@hotmail.com>
2021-01-03 14:15:37 -0300

    add inputs to contact page

--- web/src/pages/contact/index.tsx
+++ web/src/pages/contact/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
+
 import './styles/contact.css';
 
 export default function Contact() {
@@ -8,8 +8,22 @@ export default function Contact() {
     <div className="contactContainer">
       <div className="contactSmooth">
         <Navbar />
-        <div className="contactFormContainer"></div>
-        <Footer />
+        <div className="contactFormContainer">
+          <div className="contactFormContainerOne">ds</div>
+          <div className="contactFormContainerTwo">
+            <div className="contactInputsContainer">
+              <span>Send your message here</span>
+              <div className="contactInput">
+                <input required type="text" placeholder="Nome" />
+                <input required type="email" placeholder="Subject" />
+                <input required type="email" placeholder="Email" />
+                <input required type="text" placeholder="Phone" />
+                <input required type="text" placeholder="Local" />
+                <textarea required placeholder="Content" />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
